bc0ba39d0c5461a92e333a30b49cc2040c05dbe5
zhuyund <zhuyund@cs.cmu.edu>
2016-01-25 15:54:53 -0500

    implemented test_svm

--- scripts/test_svm.py
+++ scripts/test_svm.py
@@ -1,24 +1,71 @@
-#!/bin/python 
+#!/usr/bin/env python
+__author__ = 'zhuyund'
 
 import numpy
 import os
 from sklearn.svm.classes import SVC
 import cPickle
 import sys
+import argparse
+
+
+def load_test_data(feat_file_path):
+    """
+    Load all test data
+    :param feat_file_path: the file that contains all features.
+    Each line represents a video. Line starts with video_name, than a '\t', than the feature vector
+    :return: X, the test feature vectors. shape=(n_sample, n_feat)
+    """
+    test_list = open("/home/ubuntu/hw1/list/test")
+    videos = set()
+    for line in test_list:
+        video = line.split()
+        videos.add(video)
+    test_list.close()
+
+    X = []
+    for line in open(feat_file_path):
+        line = line.strip()
+        if line == '-1':
+            continue
+        video, feats = line.split('\t')
+        if video not in videos:
+            continue
+
+        x = [float(t) for t in feats.split(';')]
+        X.append(x)
+
+    return X
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("modl_file", help="path of the trained svm file")
+    parser.add_argument("feat_dir", help="dir of feature files")
+    parser.add_argument("feat_dim", help="dimension of features")
+    parser.add_argument("output_file", help="path to save the prediction score")
+    args = parser.parse_args()
+
+    # load model
+    clf = cPickle.load(open(args.model_file, 'rb'))
+
+    # load data
+    feat_file_path = args.feat_dir + '/all.vectors'
+    X = load_test_data(feat_file_path)
+
+    # predict with the log probability
+    print ">> Predicting..."
+    T = clf.predict_log_proba(X)
+
+    # write results
+    outfile = open(args.output_file, 'w')
+    for scores in T:
+        score = scores[0]
+        outfile.write(score + '\n')
+    outfile.close()
+    print ">> Prediction scores written to {0}!".format(args.output_file)
 
 # Apply the SVM model to the testing videos; Output the score for each video
-
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print "Usage: {0} model_file feat_dir feat_dim output_file".format(sys.argv[0])
-        print "model_file -- path of the trained svm file"
-        print "feat_dir -- dir of feature files"
-        print "feat_dim -- dim of features; provided just for debugging"
-        print "output_file -- path to save the prediction score"
-        exit(1)
-
-    model_file = sys.argv[1]
-    feat_dir = sys.argv[2]
-    feat_dim = int(sys.argv[3])
-    output_file = sys.argv[4]
+    main()
 
